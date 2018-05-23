410988bbe153c823eb243ef2a558d307f519d93c
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2018-05-23 14:52:15 -0400

    Compute entanglement entropy

--- dipoles_dmrg.cc
+++ dipoles_dmrg.cc
@@ -53,6 +53,29 @@ int main(int argc, char* argv[]) {
     println();
     printfln("E0 = %.15f", energy);
 
+    // Entanglement entropy.
+    psi.position(N/2);
+    auto wf = psi.A(N/2)*psi.A(N/2+1);
+    auto U = psi.A(N/2);
+
+    IQTensor S, V;
+    auto spectrum = svd(wf, U, S, V);
+
+    Real SvN = 0.0;
+    Real S2 = 0.0;
+    for(auto p : spectrum.eigs()) {
+        if(p < 1e-12) {
+            continue;
+        }
+
+        SvN += -p*log(p);
+        S2 += p*p;
+    }
+    S2 = -log(S2);
+
+    printfln("SvN = %.15f", SvN);
+    printfln("S2 = %.15f", S2);
+
     // Orientational correlation.
     auto OC_ampo = AutoMPO(sites);
     for (auto i : range1(N)) {
